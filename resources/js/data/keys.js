3a37fec9197fb22a8c62d012d2738d9e9620da23
Bob Makhlin <borismakhlin5@gmail.com>
2020-09-15 08:42:45 +0300

    Create "contact us" line

---
+++ resources/js/data/keys.js
@@ -0,0 +1,3 @@
+
+export const mapboxKey = 'pk.eyJ1IjoiYm9ibWFraGxpbiIsImEiOiJja2R1bThxa2YyYTF6Mnd0dnFsdDczdTl6In0.nbz6CtFPK23lcDJgkAEyXQ';
+
--- resources/js/my-index.js
+++ resources/js/my-index.js
@@ -1,9 +1,11 @@
+import { mapboxKey } from './data/keys.js';
 import plans from './data/plans.js';
 
 import createPlan from './components/plan/renderer.js';
 import initPlan from './components/plan/initializer.js';
 
 
+const map = L.map('map').setView([51.505, -0.09], 13);
 const nPricingsRow = document.querySelector('#pricings-row');
 
 
@@ -16,14 +18,28 @@ function init() {
 
         let nPlan = createPlan(item);
         initPlan(nPlan);
-        
+
         let nColumn = document.createElement('div');
         nColumn.className = 'col-sm-12 col-md-6 col-lg-4 mb-4';
         nColumn.append(nPlan);
 
-        
+
         nPricingsRow.append(nColumn);
 
+        initMap();
+
     }
+
+}
+
+function initMap() {
+
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+        maxZoom: 18,
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken: mapboxKey
+    }).addTo(map);
 
 }
